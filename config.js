38a9dc405813e4236676400428db4922b33138e7
Andex7 <164794457+Andex7@users.noreply.github.com>
2024-08-11 21:00:33 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,4 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
+ALIVE_IMG: process.evn.ALIVE_IMG || "https://telegra.ph/file/c8be6c96d4c1ac33b4681.jpg",
+ALIVE_MSG: process.env.ALIVE_MSG || "Hello, I am ANDEX-MD I alive now 🎉",
 };
